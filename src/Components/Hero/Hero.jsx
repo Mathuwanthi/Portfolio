de15b86b43e3b76bb3095e3addb83aaa0d1abed7
Mathuwanthi <mathuwanthi00@gmail.com>
2024-11-20 18:29:13 +0530

    Navbar-modify

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile.jpg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
         <img src={profile_img} alt=""/>
         <h1><span>I'm Mathuwanthi Navakumar,</span> frontend developer based in Sri Lanka.</h1>
         <p>I am a frontend developer from Sri Lanka , with 10 years of experience in multiple companies like Microsoft,Tesla and Apple.</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect with me</div>
+            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
             <div className="hero-resume">My resume</div>
         </div>
     </div>
